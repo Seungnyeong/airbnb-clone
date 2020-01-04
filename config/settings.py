076040e67fa8d7308fe1637a54417ba3246c3d8a
mellow0410@naver.com <mellow0410@gmail.com>
2020-01-04 23:37:34 +0900

    rooms

--- config/settings.py
+++ config/settings.py
@@ -39,11 +39,15 @@ DJANGO_APPS = [
     "django.contrib.staticfiles",
 ]
 
-PROJECT_APPS = ["users.apps.UsersConfig"]
+PROJECT_APPS = [
+    "core.apps.CoreConfig",
+    "users.apps.UsersConfig",
+    "rooms.apps.RoomsConfig",
+]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
 
-THRID_PARTY_APPS = []
+THRID_PARTY_APPS = ["django-countries"]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
--- users/admin.py
+++ users/admin.py
@@ -27,4 +27,3 @@ class CustomUserAdmin(UserAdmin):
             },
         ),
     )
-
