ac5a98672aa590bd14f7f5a1bf1e19d6ec6e13ab
mellow0410@naver.com <mellow0410@gmail.com>
2020-01-10 01:17:37 +0900

    Complete all migrations

--- rooms/models.py
+++ rooms/models.py
@@ -1,10 +1,47 @@
 from django.db import models
 from django_countries.fields import CountryField
 from core import models as core_models
-from users import models as user_models
-
 
 # Create your models here.
+
+
+class AbstractItem(core_models.TimeStampedModel):
+    """Abstrcat Item """
+
+    name = models.CharField(max_length=80)
+
+    class Meta:
+        abstract: True
+
+    def __str__(self):
+        return self.name
+
+
+class RoomType(AbstractItem):
+    class Meta:
+        verbose_name_plural = "Room Types"
+        ordering = ["name"]
+
+
+class Amenity(AbstractItem):
+    """Amentity Model Definition"""
+
+    class Meta:
+        verbose_name_plural = "Amenities"
+
+
+class Facility(AbstractItem):
+    """Facility Model Definition"""
+
+    class Meta:
+        verbose_name_plural = "Facilities"
+
+
+class HouseRule(AbstractItem):
+    """house Rule Model Definition"""
+
+    class Meta:
+        verbose_name_plural = "House Rules"
 
 
 class Room(core_models.TimeStampedModel):
@@ -21,4 +58,22 @@ class Room(core_models.TimeStampedModel):
     check_in = models.TimeField()
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
-    host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    host = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    room_type = models.ForeignKey("RoomType", on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField("Amenity", blank=True)
+    facilities = models.ManyToManyField("Facility", blank=True)
+    house_rules = models.ManyToManyField("HouseRule", blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Photo(core_models.TimeStampedModel):
+    """Photo model Definition"""
+
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.caption
--- users/models.py
+++ users/models.py
@@ -40,3 +40,7 @@ class User(AbstractUser):
     curruncy = models.CharField(choices=CURRENCY_CHOICES, max_length=3, blank=True)
 
     superhost = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
+
