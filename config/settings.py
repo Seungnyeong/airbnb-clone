216b8d45333583252a892530043b7fed1575aec4
mellow0410@naver.com <mellow0410@gmail.com>
2020-02-03 02:44:57 +0900

    filtering

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "0wep^it$)sgqqcn-oz*ryq+8$6j4r!l28vge6)mtpg3bm8=u)!"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = "*"
+ALLOWED_HOSTS = []
 
 
 # Application definition
